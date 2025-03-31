beb13cf6eddd12e193d03b3bf6be567492c2fa8d
Shikhachaudhary00 <138358483+Shikhachaudhary00@users.noreply.github.com>
2025-03-31 13:02:29 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Shikha-chaudhary-Portfolio/", 
+  base: "/shikha-chaudhary-portfolio/", // Set this to your repo name
 });
